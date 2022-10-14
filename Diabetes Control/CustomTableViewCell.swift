d3bfe8e4dfb1a0fd23fb74cc42511ce759c95153
Hiago <hiagosmd@hotmail.com>
2022-10-14 19:11:20 -0300

    Project created

---
+++ Diabetes Control/CustomTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTableViewCell.swift
+//  Diabetes Control
+//
+//  Created by Marcus Monteiro on 14/10/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Diabetes Control/Model.swift
@@ -0,0 +1,27 @@
+//
+//  Model.swift
+//  Diabetes Control
+//
+//  Created by Hiago Santos Martins Dias on 14/10/22.
+//
+
+import Foundation
+
+
+struct Model {
+    let title: String
+    let dateAndTime: String
+    let type: String
+    
+    
+    init(title: String, dateAndTime: String, type: String) {
+        self.title = title
+        self.dateAndTime = dateAndTime
+        self.type = type
+    }
+    
+}
+
+
+
+
